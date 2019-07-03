0ffb582e27cc1c0423be152d68d98218fd8ee7c5
karthik <karthik.n@prodapt.com>
2019-07-03 13:07:08 +0530

    container and presentation component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
-function App() {
-  return (
+import { DISHES } from './commmon-utils/dishes';
+import Main from './components/MainComponent';
+class App extends Component {
+  
+  render()
+  {
+  return(
     <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
-      </div>
+    <Main />
+  </div>
   );
+}
 }
 
 export default App;
